877e1602347b076b22095fe0b8eaf6aa1c7aae8e
nttdatasvq <nttdata.formacion@nttdata.com>
2023-02-17 13:32:42 +0100

    Primer commit

---
+++ src/com/nttdata/git/App.java
@@ -0,0 +1,23 @@
+package com.nttdata.git;
+
+/**
+ * Formación de GIT
+ * 
+ * @author NTT Data
+ *
+ */
+public class App {
+
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Saludo.
+		System.out.println("Hola NTT Data");
+
+	}
+
+}
